c136c869251f008ef68b9d3c78d44b0052900d22
Diogo Correia <me@diogotc.com>
2021-06-24 11:51:45 +0100

    Correctly handle static assets on build

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use enigo::{Enigo, KeyboardControllable};
-use rocket::fs::{FileServer, relative};
+use rocket::fs::{FileServer};
 use rocket::{post, launch, routes};
+use std::env;
 
 #[post("/paste", data = "<input>")]
 fn paste_text(input: &str) {
@@ -10,7 +11,13 @@ fn paste_text(input: &str) {
 
 #[launch]
 fn rocket() -> _ {
+    let mut path = env::current_dir().ok().expect("Failed to get current path");
+    path.push("src");
+    path.push("static");
+
+    println!("Reading static assets from {}", path.display());
+
     rocket::build()
       .mount("/", routes![paste_text])
-      .mount("/", FileServer::from(relative!("src/static")))
+      .mount("/", FileServer::from(path))
 }
